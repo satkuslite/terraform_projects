6e596bbb8dc66fee27ffdc1937f5dd3437814242
satkuslite <126190454+satkuslite@users.noreply.github.com>
2023-07-13 15:36:19 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,24 +34,6 @@ resource "aws_subnet" "subnet_b" {
   availability_zone       = "us-east-1b"
 }
 
-# Declare a data source that retrieves the most recent Amazon Machine Image (AMI) 
-
- data "aws_ami" "ubuntu" {
-   most_recent = true
- 
-   filter {
-     name = "name"
-     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-   }
- 
-   filter {
-     name = "virtualization-type"
-     values = ["hvm"]
-   }
- 
-   owners = ["099720109477"] # Canonical
- }
-
 # Security group
 
  resource "aws_security_group" "security_terraform" {
@@ -124,14 +106,6 @@ resource "aws_subnet" "subnet_b" {
      instance_protocol = "http"
      lb_port = 80
      lb_protocol = "http"
-   }
- 
-   health_check {
-     healthy_threshold = 2
-     unhealthy_threshold = 2
-     timeout = 3
-     target = "HTTP:80/"
-     interval = 30
    }
    
    cross_zone_load_balancing = true
